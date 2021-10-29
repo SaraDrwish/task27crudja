3c6bdb906808a3261ecae76058a45b0c2ef3374b
Sara <38770404+SaraDrwish@users.noreply.github.com>
2021-10-30 01:26:57 +0300

    radwa task push edit

--- main.js
+++ main.js
@@ -1,59 +1,39 @@
+var tasks = [];
 
+function add(){
+    var task = document.getElementById("task").value ;
+    tasks.push(task);
+    document.getElementById("task").value = "";
+    view();
+}
 
-// var tasks = [];
-// function add(){
+function view(){
+    var list = "";
+    var count = tasks.length;
+    for(var i = 0 ; i< count; i++ ){
+       list += "<li ondblclick = 'destroy( " + i + ") '> " + tasks[i] + "</li> <span style='color:coral;' onclick='update("+ i +")'>update</span>" 
+    }
+    document.getElementById("res").innerHTML= list;
+}
 
-//     var task = document.getElementById("task").value ;
-//     tasks.push(task);
-//     document.getElementById("task").value = "";
+function destroy(index){
+    tasks.splice(index , 1);
+    view();
+}
 
-//     view();
-// }
+function update(edit){
+    var task = document.getElementById("task").value=tasks[edit];
+    var update = document.getElementById("update").style.display="inline-block";
+    var add = document.getElementById("add").style.display="none";
+    var edit = document.getElementById("edit").value=edit ;
+}
 
-// function view(){
-
-//     var list = "";
-//     var count = tasks.length;
-//     for(var i = 0 ; i< count; i++ ){
-//  list += "<li ondblclick = 'destroy( " + i + ") '> " + tasks[i] + "</li> <span style='color:coral;' onclick='update("+ i +")'>update</span>" 
-//     }
- 
-//     document.getElementById("res").innerHTML= list;
-
-// }
-
-
-
-
-
-
-// function destroy(index){
-    
-//     tasks.splice(index , 1);
-
-//     view();
-// }
-
-// function update(edit){
-
-// var task = document.getElementById("task").value=tasks[edit];
-// var update = document.getElementById("update").style.display="inline-block";
-// var add = document.getElementById("add").style.display="none";
-// var edit = document.getElementById("edit").value=edit ;
-
-// }
-
-// function edit(){
-// var task = document.getElementById("task").value;
-// var edit = document.getElementById("edit").value;
-// tasks[edit]= task;
-// document.getElementById("task").value = "";
-// var update = document.getElementById("update").style.display="none";
-// var add = document.getElementById("add").style.display="inline-block";
-
-// view();
-
-// }
-
-
-
+function edit(){
+    var task = document.getElementById("task").value;
+    var edit = document.getElementById("edit").value;
+    tasks[edit]= task;
+    document.getElementById("task").value = "";
+    var update = document.getElementById("update").style.display="none";
+    var add = document.getElementById("add").style.display="inline-block";
+view();
+}
--- taskradwajs24/main.js
+++ taskradwajs24/main.js
@@ -1,18 +1,57 @@
+ var listbox=[];
+// var box = document.getElementById("box");
 
-var listbox=[];
+function add( ){
 
-function add(){
-
-var pname = document.getElementById("pname").value ;
+var pname = document.getElementById("pname").value  ;
 var ppric = document.getElementById("ppric").value ;
 var pcatgo = document.getElementById("pcatgo").value ;
 var pdesc= document.getElementById("pdesc").value ;
+var text1 = document.getElementById("text1");
+var text2= document.getElementById("text2");
+var text3 = document.getElementById("text3");
+var text4 = document.getElementById("text4");
+ 
+    var temp = "";
+    var count = listbox.length;
 
+                    for(var i = 0 ; i<count  ; i++ )
+                    {
 
-var count =list.length;
-for(var i = 0 ; i< count ; i ++ ){
+                        
+                                temp +=
+                        ` 
+                        <div class="innerbox" id="innerbox">
+                        <img src= "https://elzerowebschool.github.io/HTML_And_CSS_Template_Two/images/shuffle-06.jpg"  alt="gg"></img>
+                        <span id="text1"> `+pname +` </span> 
+                        <span id="text2"> `+ppric +` </span>
+                        <p id="text3" >  `+ pdesc+`  </p>
+                        <span id="text4" > `+ pcatgo +`</span>
+                        </div>
+                        `   
 
+                       
+
+                        // var temp = document.getElementById("temp").value ;
+
+                    }
+
+                    var pname = document.getElementById("pname").value=""  ;
+                    var ppric = document.getElementById("ppric").value ="";
+                    var pcatgo = document.getElementById("pcatgo").value ="";
+                    var pdesc= document.getElementById("pdesc").value="" ;
+
+// box = document.getElementById("box").style.display="none";
+document.getElementById("box").innerHTML= temp;
+listbox.push(temp);
 
 }
+add( );
+// function veiw(){
+//     var temp = document.getElementById("temp").value ;
+//     listbox.push(temp);
+//     document.getElementById("temp").value = "";
 
-}
+//     add();
+// }
+
