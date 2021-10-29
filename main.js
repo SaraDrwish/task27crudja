b4709e41f4b64a2357174bf4902d9867985fe976
Sara <38770404+SaraDrwish@users.noreply.github.com>
2021-10-29 22:29:38 +0300

    radwa task trying

--- main.js
+++ main.js
@@ -1,51 +1,59 @@
 
 
-var tasks = [];
-function add(){
+// var tasks = [];
+// function add(){
 
-    var task = document.getElementById("task").value ;
-    tasks.push(task);
-    document.getElementById("task").value = "";
+//     var task = document.getElementById("task").value ;
+//     tasks.push(task);
+//     document.getElementById("task").value = "";
 
-    view();
-}
+//     view();
+// }
 
-function view(){
+// function view(){
 
-    var list = "";
-    var count = tasks.length;
-    for(var i = 0 ; i< count; i++ ){
- list += "<li ondblclick = 'destroy( " + i + ") '> " + tasks[i] + "</li> <span style='color:coral;' onclick='update("+ i +")'>update</span>" 
-    }
+//     var list = "";
+//     var count = tasks.length;
+//     for(var i = 0 ; i< count; i++ ){
+//  list += "<li ondblclick = 'destroy( " + i + ") '> " + tasks[i] + "</li> <span style='color:coral;' onclick='update("+ i +")'>update</span>" 
+//     }
  
-    document.getElementById("res").innerHTML= list;
+//     document.getElementById("res").innerHTML= list;
 
-}
+// }
 
-function destroy(index){
+
+
+
+
+
+// function destroy(index){
     
-    tasks.splice(index , 1);
+//     tasks.splice(index , 1);
 
-    view();
-}
+//     view();
+// }
 
-function update(edit){
+// function update(edit){
 
-var task = document.getElementById("task").value=tasks[edit];
-var update = document.getElementById("update").style.display="inline-block";
-var add = document.getElementById("add").style.display="none";
-var edit = document.getElementById("edit").value=edit ;
+// var task = document.getElementById("task").value=tasks[edit];
+// var update = document.getElementById("update").style.display="inline-block";
+// var add = document.getElementById("add").style.display="none";
+// var edit = document.getElementById("edit").value=edit ;
 
-}
+// }
 
-function edit(){
-var task = document.getElementById("task").value;
-var edit = document.getElementById("edit").value;
-tasks[edit]= task;
-document.getElementById("task").value = "";
-var update = document.getElementById("update").style.display="none";
-var add = document.getElementById("add").style.display="inline-block";
+// function edit(){
+// var task = document.getElementById("task").value;
+// var edit = document.getElementById("edit").value;
+// tasks[edit]= task;
+// document.getElementById("task").value = "";
+// var update = document.getElementById("update").style.display="none";
+// var add = document.getElementById("add").style.display="inline-block";
 
-view();
+// view();
 
-}
+// }
+
+
+
---
+++ taskradwajs24/main.js
@@ -0,0 +1,18 @@
+
+var listbox=[];
+
+function add(){
+
+var pname = document.getElementById("pname").value ;
+var ppric = document.getElementById("ppric").value ;
+var pcatgo = document.getElementById("pcatgo").value ;
+var pdesc= document.getElementById("pdesc").value ;
+
+
+var count =list.length;
+for(var i = 0 ; i< count ; i ++ ){
+
+
+}
+
+}
