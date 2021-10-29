efda9016b32f2249874c710d86303f04f12c2771
Sara <38770404+SaraDrwish@users.noreply.github.com>
2021-10-29 21:05:07 +0300

    thedit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-var tasks = [];
 
+var tasks = [];
 function add(){
 
     var task = document.getElementById("task").value ;
@@ -14,10 +14,8 @@ function view(){
 
     var list = "";
     var count = tasks.length;
-
     for(var i = 0 ; i< count; i++ ){
-
-        list += "<li ondblclick = 'destroy( " + i + ")   '> "   +  tasks[i] + "</li>" ;  
+ list += "<li ondblclick = 'destroy( " + i + ") '> " + tasks[i] + "</li> <span style='color:coral;' onclick='update("+ i +")'>update</span>" 
     }
  
     document.getElementById("res").innerHTML= list;
@@ -27,18 +25,27 @@ function view(){
 function destroy(index){
     
     tasks.splice(index , 1);
+
     view();
 }
 
+function update(edit){
 
-
-function update(){
-
+var task = document.getElementById("task").value=tasks[edit];
+var update = document.getElementById("update").style.display="inline-block";
+var add = document.getElementById("add").style.display="none";
+var edit = document.getElementById("edit").value=edit ;
 
 }
 
-
 function edit(){
+var task = document.getElementById("task").value;
+var edit = document.getElementById("edit").value;
+tasks[edit]= task;
+document.getElementById("task").value = "";
+var update = document.getElementById("update").style.display="none";
+var add = document.getElementById("add").style.display="inline-block";
 
-    
+view();
+
 }
