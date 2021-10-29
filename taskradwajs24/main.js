298b3d0e21eb6deaef8cd3a4804009bdf701232c
Sara <38770404+SaraDrwish@users.noreply.github.com>
2021-10-30 01:33:53 +0300

    radwa task final edit push and + delete some commints

--- taskradwajs24/main.js
+++ taskradwajs24/main.js
@@ -1,25 +1,14 @@
  var listbox=[];
-// var box = document.getElementById("box");
-
 function add( ){
-
 var pname = document.getElementById("pname").value  ;
 var ppric = document.getElementById("ppric").value ;
 var pcatgo = document.getElementById("pcatgo").value ;
 var pdesc= document.getElementById("pdesc").value ;
-var text1 = document.getElementById("text1");
-var text2= document.getElementById("text2");
-var text3 = document.getElementById("text3");
-var text4 = document.getElementById("text4");
- 
     var temp = "";
     var count = listbox.length;
-
                     for(var i = 0 ; i<count  ; i++ )
                     {
-
-                        
-                                temp +=
+                                temp =
                         ` 
                         <div class="innerbox" id="innerbox">
                         <img src= "https://elzerowebschool.github.io/HTML_And_CSS_Template_Two/images/shuffle-06.jpg"  alt="gg"></img>
@@ -29,29 +18,15 @@ var text4 = document.getElementById("text4");
                         <span id="text4" > `+ pcatgo +`</span>
                         </div>
                         `   
-
-                       
-
-                        // var temp = document.getElementById("temp").value ;
-
                     }
 
-                    var pname = document.getElementById("pname").value=""  ;
-                    var ppric = document.getElementById("ppric").value ="";
-                    var pcatgo = document.getElementById("pcatgo").value ="";
-                    var pdesc= document.getElementById("pdesc").value="" ;
+var pname = document.getElementById("pname").value=""  ;
+var pcatgo = document.getElementById("pcatgo").value ="";
+var pdesc= document.getElementById("pdesc").value="" ;
+var ppric= document.getElementById("ppric").value="" ;
 
-// box = document.getElementById("box").style.display="none";
-document.getElementById("box").innerHTML= temp;
+document.getElementById("box").innerHTML+= temp;
 listbox.push(temp);
-
 }
 add( );
-// function veiw(){
-//     var temp = document.getElementById("temp").value ;
-//     listbox.push(temp);
-//     document.getElementById("temp").value = "";
-
-//     add();
-// }
 
